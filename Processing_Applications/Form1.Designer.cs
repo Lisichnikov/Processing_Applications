6952c2de39fe3be0b30c28e206fa32a3c59d3a37
Алексей Лисичников <alexochanel@gmail.com>
2023-11-22 08:43:39 +0300

    Программа готова

--- Processing_Applications/Form1.Designer.cs
+++ Processing_Applications/Form1.Designer.cs
@@ -35,6 +35,18 @@
             this.open_button = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -42,7 +54,7 @@
             this.richTextBox1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox1.Font = new System.Drawing.Font("Golos Text Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.richTextBox1.Location = new System.Drawing.Point(29, 35);
+            this.richTextBox1.Location = new System.Drawing.Point(27, 32);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(579, 475);
             this.richTextBox1.TabIndex = 0;
@@ -56,7 +68,7 @@
             this.confirm_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.confirm_button.Font = new System.Drawing.Font("Golos Text DemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.confirm_button.ForeColor = System.Drawing.Color.SeaGreen;
-            this.confirm_button.Location = new System.Drawing.Point(630, 325);
+            this.confirm_button.Location = new System.Drawing.Point(628, 322);
             this.confirm_button.Name = "confirm_button";
             this.confirm_button.Size = new System.Drawing.Size(273, 84);
             this.confirm_button.TabIndex = 3;
@@ -72,7 +84,7 @@
             this.reject_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.reject_button.Font = new System.Drawing.Font("Golos Text DemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.reject_button.ForeColor = System.Drawing.Color.Firebrick;
-            this.reject_button.Location = new System.Drawing.Point(630, 426);
+            this.reject_button.Location = new System.Drawing.Point(628, 423);
             this.reject_button.Name = "reject_button";
             this.reject_button.Size = new System.Drawing.Size(273, 84);
             this.reject_button.TabIndex = 2;
@@ -88,7 +100,7 @@
             this.open_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.open_button.Font = new System.Drawing.Font("Golos Text DemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.open_button.ForeColor = System.Drawing.Color.Goldenrod;
-            this.open_button.Location = new System.Drawing.Point(630, 35);
+            this.open_button.Location = new System.Drawing.Point(628, 32);
             this.open_button.Name = "open_button";
             this.open_button.Size = new System.Drawing.Size(273, 84);
             this.open_button.TabIndex = 1;
@@ -100,19 +112,118 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(936, 562);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.confirm_button);
+            this.tabPage1.Controls.Add(this.open_button);
+            this.tabPage1.Controls.Add(this.reject_button);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(928, 533);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Данные конкурсанта";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(928, 533);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Работа конкурсанта";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::Processing_Applications.Properties.Resources.yellow;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Golos Text DemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.button1.Location = new System.Drawing.Point(627, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 84);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(31, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(557, 472);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::Processing_Applications.Properties.Resources.green;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Golos Text DemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.SeaGreen;
+            this.button2.Location = new System.Drawing.Point(627, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(273, 84);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Принять";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = global::Processing_Applications.Properties.Resources.red;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Golos Text DemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.Firebrick;
+            this.button3.Location = new System.Drawing.Point(627, 412);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(273, 84);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Отклонить";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(933, 546);
-            this.Controls.Add(this.confirm_button);
-            this.Controls.Add(this.reject_button);
-            this.Controls.Add(this.open_button);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(937, 564);
+            this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Обработка конкурсных заявок";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -125,6 +236,14 @@
         private System.Windows.Forms.Button confirm_button;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Processing_Applications/Form1.cs
+++ Processing_Applications/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Processing_Applications
@@ -20,8 +21,6 @@ namespace Processing_Applications
 
             openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
             saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
-
-            string Text = "";
         }
 
         private void open_button_Click(object sender, EventArgs e)
@@ -34,14 +33,14 @@ namespace Processing_Applications
             string fileText = System.IO.File.ReadAllText(filename);
             richTextBox1.Text = fileText;
             MessageBox.Show("Файл открыт", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            File.Delete(filename);
+            // File.Delete(filename);
         }
 
         private void confirm_button_Click(object sender, EventArgs e)
         {
             if (richTextBox1.Text == "")
             {
-                MessageBox.Show("Пусто!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Пусто!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
@@ -54,7 +53,7 @@ namespace Processing_Applications
                 // сохраняем текст в файл
                 saveFileDialog1.FileName = Text;
                 System.IO.File.WriteAllText(filename, richTextBox1.Text);
-                MessageBox.Show("Файл был принят!", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Файл был отклонен!", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 richTextBox1.Text = "";
             }
         }
@@ -63,7 +62,7 @@ namespace Processing_Applications
         {
             if (richTextBox1.Text == "")
             {
-                MessageBox.Show("Пусто!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Пусто!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
@@ -78,6 +77,88 @@ namespace Processing_Applications
                 System.IO.File.WriteAllText(filename, richTextBox1.Text);
                 MessageBox.Show("Файл был принят!", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 richTextBox1.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.Multiselect = true;
+            openFileDialog2.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.FileStream fs = new System.IO.FileStream(openFileDialog2.FileName, System.IO.FileMode.Open);
+                System.Drawing.Image img = System.Drawing.Image.FromStream(fs);
+                fs.Close();
+                pictureBox1.Image = img;
+                MessageBox.Show("Файл открыт", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Файл не был открыт", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null) //если в pictureBox есть изображение
+            {
+                //создание диалогового окна "Сохранить как..", для сохранения изображения
+                SaveFileDialog savedialog = new SaveFileDialog();
+                savedialog.Title = "Сохранить картинку как...";
+                //отображать ли предупреждение, если пользователь указывает имя уже существующего файла
+                savedialog.OverwritePrompt = true;
+                //отображать ли предупреждение, если пользователь указывает несуществующий путь
+                savedialog.CheckPathExists = true;
+                //список форматов файла, отображаемый в поле "Тип файла"
+                savedialog.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+                //отображается ли кнопка "Справка" в диалоговом окне
+                savedialog.ShowHelp = true;
+                if (savedialog.ShowDialog() == DialogResult.OK) //если в диалоговом окне нажата кнопка "ОК"
+                {
+                    try
+                    {
+                        pictureBox1.Image.Save(savedialog.FileName);
+                        MessageBox.Show("Файл был принят!", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null) //если в pictureBox есть изображение
+            {
+                //создание диалогового окна "Сохранить как..", для сохранения изображения
+                SaveFileDialog savedialog = new SaveFileDialog();
+                savedialog.Title = "Сохранить картинку как...";
+                //отображать ли предупреждение, если пользователь указывает имя уже существующего файла
+                savedialog.OverwritePrompt = true;
+                //отображать ли предупреждение, если пользователь указывает несуществующий путь
+                savedialog.CheckPathExists = true;
+                //список форматов файла, отображаемый в поле "Тип файла"
+                savedialog.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+                //отображается ли кнопка "Справка" в диалоговом окне
+                savedialog.ShowHelp = true;
+                if (savedialog.ShowDialog() == DialogResult.OK) //если в диалоговом окне нажата кнопка "ОК"
+                {
+                    try
+                    {
+                        MessageBox.Show("Файл был отклонен!", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        pictureBox1.Image.Save(savedialog.FileName);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
             }
         }
     }
